2369b5912438fe435a5398f0e0fe3b1d845d16eb
JandersonMota00 <20231tadssaj@ifba.edu.br>
2025-07-22 12:31:15 -0300

    Estrutura MVC do projeto

---
+++ src/test/java/com/sgvv/ifba/IfbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgvv.ifba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
