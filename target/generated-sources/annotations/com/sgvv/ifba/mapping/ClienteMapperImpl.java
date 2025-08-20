36bc10832df1c9a23de6e1298d5b4274c51ba84c
Ronaldo <20222tadssaj0007@ifba.edu.br>
2025-08-20 15:33:43 -0300

    att branch Ronaldo-Correia

--- target/generated-sources/annotations/com/sgvv/ifba/mapping/ClienteMapperImpl.java
+++ target/generated-sources/annotations/com/sgvv/ifba/mapping/ClienteMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-16T16:13:51-0300",
+    date = "2025-08-20T15:33:17-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.6 (Oracle Corporation)"
 )
 @Component
